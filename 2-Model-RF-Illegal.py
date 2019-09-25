3205257410636f48f9205a7ca9056efd6fce9bb7
johnwoodill <johnwoodill@gmail.com>
2019-09-24 18:21:13 -0700

    updating figures

---
+++ 2-Model-RF-Illegal.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+from sklearn.preprocessing import MinMaxScaler
+from statsmodels.discrete.discrete_model import Logit
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import average_precision_score, accuracy_score, roc_curve, auc, precision_recall_curve, f1_score
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.metrics as metrics
+from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit, GridSearchCV
+from collections import deque
+import calendar
+
+#Fully processed data
+# dat.to_feather('data/full_gfw_10d_illegal_model_data_DAILY_2012-01-01_2016-12-31.feather')
+
+# 8Day data
+dat = pd.read_feather('~/Projects/Seascape-and-fishing-effort/data/full_gfw_10d_effort_model_data_8DAY_2012-01-01_2016-12-26.feather')
+
+# DAILY data
+#dat = pd.read_feather('~/Projects/predicting-illegal-fishing/data/full_gfw_10d_illegal_model_data_DAILY_2012-01-01_2016-12-31.feather')
+
+# Subset drifting longlines
+#dat = dat[dat.geartype == 'drifting_longlines']
+
+# Keep only vessel with fishing hours
+#dat = dat[dat['fishing_hours'] > 0 ]
+
+# Keep only Chinese vessels
+dat = dat[(dat['flag'] == 'CHN') | (dat['flag'] == 'ARG')]
+
+# If illegally operating inside EEZ (!= ARG)
+dat.loc[:, 'illegal'] = np.where(((dat['eez'] == True) & (dat['fishing_hours'] > 0) & (dat['flag'] != 'ARG') ), 1, 0)
+
+# Illegal if within 50km
+dat.loc[:, 'illegal_50km'] = np.where(((dat['eez'] == True) & (dat['fishing_hours'] > 0 ) & (dat['distance_to_eez_km'] <= 50) & (dat['flag'] != 'ARG')), 1, 0)
+
+# Illegal if within 100km
+dat.loc[:, 'illegal_100km'] = np.where(((dat['eez'] == True) & (dat['fishing_hours'] > 0 ) & (dat['distance_to_eez_km'] <= 100) & (dat['flag'] != 'ARG')), 1, 0)
+
+# Convert true/false eez to 0/1
+dat.loc[:, 'illegal'] = dat.illegal.astype('uint8')
+dat.loc[:, 'illegal_50km'] = dat.illegal_50km.astype('uint8')
+dat.loc[:, 'illegal_100km'] = dat.illegal_100km.astype('uint8')
+
+sum(dat.illegal)/len(dat)
+sum(dat.illegal_50km)/len(dat)
+sum(dat.illegal_100km)/len(dat)
+
+# Get year month
+dat.loc[:, 'year'] = pd.DatetimeIndex(dat['date']).year
+dat.loc[:, 'month'] = pd.DatetimeIndex(dat['date']).month
+
+# Convert month number to name
+dat.loc[:, 'month_abbr'] = dat.apply(lambda x: calendar.month_abbr[x['month']], 1)
+
+# Linear model
+# Get data frame of variables and dummy seascapes
+moddat = dat[['illegal', 'year', 'fishing_hours', 'month_abbr', 'seascape_class', 'sst', 'sst_grad', 'chlor_a', 'lon1', 'lat1', 'depth_m', 'coast_dist_km', 'port_dist_km', 'eez', 'distance_to_eez_km']].dropna().reset_index(drop=True)
+
+# Dummy variables for seascape and dummies
+seascape_dummies = pd.get_dummies(moddat['seascape_class'], prefix='seascape').reset_index(drop=True)
+month_dummies = pd.get_dummies(moddat['month_abbr']).reset_index(drop=True)
+
+# Concat dummy variables
+moddat = pd.concat([moddat, seascape_dummies, month_dummies], axis=1)
+
+# Get X, y
+y = moddat[['year', 'illegal']].reset_index(drop=True)
+
+# Drop dummy variables and prediction
+moddat = moddat.drop(columns = ['month_abbr', 'illegal', 'seascape_class'])
+
+# Build data for model
+X = moddat
+X.columns
+X.head()
+y.head()
+
+# Cross-validate model
+roc_dat = pd.DataFrame()
+tpr_fpr = pd.DataFrame()
+feffort = pd.DataFrame()
+sdat = pd.DataFrame()
+for year in range(2012, 2017):
+    
+    # Get training data
+    X_train = X[X.year != year]
+    y_train = y[y.year != year]
+
+    # Get test data
+    X_test = X[X.year == year]
+    y_test = y[y.year == year]
+
+    print(f"Training Years: {X_train.year.unique()} - Test Year: {X_test.year.unique()}")
+
+    fishing_hours = X_test['fishing_hours']
+
+    # Drop year
+    X_train = X_train.drop(columns = ['year'])
+    X_test = X_test.drop(columns = ['year'])
+
+    # Set binary variable
+    y_train = y_train['illegal']
+    y_test = y_test['illegal']
+    
+    #clf = LogisticRegression().fit(X_train, y_train)
+    clf = RandomForestClassifier(n_estimators=100).fit(X_train, y_train)
+
+    # Get predicted probabilities for sensitivity analysis
+    pred_proba = clf.predict_proba(X_test)
+    proba = pred_proba[:, 1]
+
+    # Get predictions and fishing hours
+    y_pred = clf.predict(X_test)
+
+    # data frame of true, pred, and fishing hours
+    pred_dat = pd.DataFrame({'y_true': y_test, 'y_pred': y_pred, 'fishing_hours': fishing_hours})
+    
+    tf_dat = pred_dat[pred_dat['y_true'] == 1]
+    tf = sum(tf_dat['fishing_hours'])
+
+    # True positive
+    fh_tpr_dat = pred_dat[(pred_dat['y_true'] == 1) & (pred_dat['y_pred'] == 1)]
+
+    # False positive
+    fh_fpr_dat = pred_dat[((pred_dat['y_true'] == 0) & (pred_dat['y_pred'] != 0)) | ((pred_dat['y_true'] == 1) & (pred_dat['y_pred'] != 1))]
+    
+    tpr_fishing_hours = sum(fh_tpr_dat['fishing_hours'])
+    fpr_fishing_hours = sum(fh_fpr_dat['fishing_hours'])
+    total_fishing_hours = sum(pred_dat['fishing_hours'])
+
+    feffort_indat = pd.DataFrame({'year': [year], 'total_fishing': [tf], 'tpr_fishing': [tpr_fishing_hours], 'fpr_fishing_hours': [fpr_fishing_hours]})
+    feffort = pd.concat([feffort, feffort_indat])
+    
+    # Precision-recall across thresholds
+    # Precision = tp / ( tp + fp )
+    # Recall = tn / ( tn + fp )
+    precision, recall, thresholds = precision_recall_curve(y_test, proba )
+
+    # Test predictions
+    y_pred = clf.predict(X_test)
+
+    # F1 score = 2 * precision * recall / precision + recall
+    f1 = f1_score(y_test, y_pred)
+
+    # calculate precision-recall AUC
+    auc_m = auc(recall, precision)
+
+    # calculate average precision score
+    ap = average_precision_score(y_test, proba)
+    print('f1=%.3f auc=%.3f ap=%.3f' % (f1, auc_m, ap))
+
+    ddat = pd.DataFrame({'year': year, 'prec': precision, 'recall': recall, 'f1': f1, 'auc':auc_m, 'ap':ap})
+
+    sdat = pd.concat([sdat, ddat])
+
+# Save fishing hours data
+feffort = feffort.reset_index(drop=True)
+feffort = feffort.to_feather('data/predicted_effort_data.feather')
+
+# Save precision-recall data
+sdat = sdat.reset_index(drop=True)
+sdat.to_feather('data/illegal_cross_val_dat.feather')
+
+# Feature importance
+fea_import = pd.DataFrame({'variable': X_train.columns , 'importance': clf.feature_importances_})
+fea_import = fea_import.sort_values('importance', ascending=False)
+fea_import = fea_import.reset_index(drop=True)
+fea_import.to_feather('data/feature_importance_rf_illegal.feather')
+
+sns.barplot('importance', 'variable', data = fea_import)
+plt.show()
+
---
+++ 3-Figures.R
@@ -0,0 +1,169 @@
+library(tidyverse)
+library(ggthemes)
+library(feather)
+library(viridis)
+library(lubridate)
+library(stringr)
+
+# Full data
+dat <- read_feather('~/Projects/predicting-illegal-fishing/data/full_gfw_10d_effort_model_data_8DAY_2012-01-01_2016-12-26.feather')
+
+# Model results
+mdat <- read_feather('~/Projects/predicting-illegal-fishing/data/illegal_cross_val_dat.feather')
+
+# Feature importance
+fea <- read_feather('~/Projects/predicting-illegal-fishing/data/feature_importance_rf_illegal.feather')
+
+# Fishing Effort predictions
+fe <- read_feather('~/Projects/predicting-illegal-fishing/data/predicted_effort_data.feather')
+
+
+
+
+# ------------------------------------------------------------------------------------
+# Figure ***
+dat$year <- year(dat$date)
+dat$month <- month(dat$date)
+dat$year_month <- paste0(dat$month, "-", dat$year)
+dat <- filter(dat, fishing_hours > 0)
+dat <- filter(dat, flag %in% c("ARG", "CHN"))
+dat$illegal <- ifelse(dat$eez == TRUE, ifelse(dat$flag != "ARG", 1, 0), 0)
+
+pdat <- dat %>% 
+  group_by(year_month) %>% 
+  summarise(year = mean(year), 
+            month = mean(month),
+            total_illegal = sum(illegal))
+
+pdat$month_name <- month.name[pdat$month]
+pdat <- arrange(pdat, month_name)
+pdat$month_name <- factor(pdat$month_name, levels = month.name)
+
+ggplot(pdat, aes(year_month, total_illegal, fill=factor(month_name))) + 
+  theme_tufte(12) +
+  labs(x=NULL, y="Count of Illegal Chinese Activity") +
+  geom_bar(stat = "identity") +
+  scale_fill_viridis_d() +
+  theme(legend.title = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  coord_flip() +
+  NULL
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------
+# Figure *** Feature Importance
+fea <- filter(fea, importance > 0)
+fea$variable <- c("Distance to EEZ",
+                  "EEZ",
+                  "Distance to Coast",
+                  "Longitude",
+                  "Distance to Port",
+                  "Latitude",
+                  "Sea Surface Temp.",
+                  "Chlorophyll",
+                  "Ocean Depth",
+                  "Sea Surface Gradient",
+                  "August",
+                  "September",
+                  "Seascape 14",
+                  "Seascape 7",
+                  "February",
+                  "December",
+                  "May",
+                  "March",
+                  "June",
+                  "July",
+                  "January",
+                  "April",
+                  "November",
+                  "October",
+                  "Seascape 21",
+                  "Seascape 15",
+                  "Seascape 27",
+                  "Seascape 17",
+                  "Seascape 3",
+                  "Seascape 12")
+
+ggplot(fea, aes(reorder(variable, importance), importance)) + 
+  geom_bar(stat='identity') +
+  theme_tufte(12) +
+  labs(x=NULL, y="Feature Importance") +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  coord_flip() +
+  NULL
+
+ggsave("~/Projects/predicting-illegal-fishing/figures/feature_importance.pdf", width = 6, height = 5)
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------
+# Figure ***
+mdat$year_label <- paste0(mdat$year, " - F1: ", round(mdat$f1, 2), " AUC: ", round(mdat$auc, 2), " AP: ", round(mdat$ap, 2))
+
+# Custom color palette
+cbp1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+          "#0072B2", "#D55E00", "#CC79A7")
+
+ggplot(mdat, aes(recall, prec, color=factor(year_label))) + 
+  labs(x="Recall", y="Precision") +
+  theme_tufte(12) +
+  # scale_color_manual(values = viridis(6, option = "D")) +
+  scale_color_manual(values = cbp1) +
+  geom_line() +
+  theme(legend.position = c(.20, .20),
+        legend.direction = 'vertical',
+        legend.justification = 'center',
+        legend.text = element_text(size=8),
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  NULL
+
+ggsave("~/Projects/predicting-illegal-fishing/figures/precision_recall.pdf", width = 6, height = 4)
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------
+# Figure *** Fishing effort explained
+
+fe <- gather(fe, key=label, value = value, -year)
+fe <- filter(fe, label != "total_fishing")
+
+fe$label <- ifelse(fe$label == "tpr_fishing", "Explained Fishing Effort", "Unexplained Fishing Effort")
+fe$label <- factor(fe$label, levels = c("Unexplained Fishing Effort", "Explained Fishing Effort"))
+
+ggplot(fe, aes(x=year, y=value, fill=factor(label))) + 
+  geom_bar(stat='identity') +
+  labs(x=NULL, y="Total Fishing Hours") +
+  theme_tufte(12) +
+  scale_fill_manual(values = c("#22A884FF", "#2A788EFF")) +
+  theme(legend.position = c(.20, .90),
+        legend.direction = 'vertical',
+        legend.justification = 'center',
+        legend.text = element_text(size=8),
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  NULL
+
+ggsave("~/Projects/predicting-illegal-fishing/figures/explained_fishing_effort.pdf", width=6, height=4)
+
+
+
+
+
